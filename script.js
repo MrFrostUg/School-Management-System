653fd9536258f2778e6c1f7d038b04dda50cce93
MrFrostUg <frostmartin882@gmail.com>
2023-11-18 20:19:07 +0100

    deleted functions

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
 // Function to add an image to the navigation bar
 function addImageToNavbar() {
-    // Create an image element
-    var image = document.createElement("img");
-    image.src = "image/icon.svg"; 
-    image.alt = "Logo";
+  // Create an image element
+  var image = document.createElement("img");
+  image.src = "image/icon.svg";
+  image.alt = "Logo";
 
-    // Add a CSS class to style the image
-    image.classList.add("navbar-logo");
+  // Add a CSS class to style the image
+  image.classList.add("navbar-logo");
 
-    // Append the image to the navbar-brand element
-    var navbarBrand = document.querySelector(".navbar-brand");
-    navbarBrand.appendChild(image);
+  // Append the image to the navbar-brand element
+  var navbarBrand = document.querySelector(".navbar-brand");
+  navbarBrand.appendChild(image);
 }
 
 // Call the function to add the image when the page loads
