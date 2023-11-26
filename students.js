5f58a528258c28cd7893a9295b12b9ecfd0ea9dc
MrFrostUg <frostmartin882@gmail.com>
2023-11-26 20:02:17 +0100

    created files for the codes

--- students.js
+++
@@ -1,7 +0,0 @@
- // Create an array of student objects
-      const students = [
-        { firstName: "Max", secondName: "Herman", grade: 3},
-        { firstName: "Anthony", secondName: "Egdbe", grade: 5 },
-        
-      ];
-    
--- teachers.js
+++
@@ -1,5 +0,0 @@
-const students = [
-    { firstName: "Hannes", secondName: "Bühler", class:"Jvascript Expert"},
-    { firstName: "Ali", secondName: "Sayar", class:"AWS Expert"},
-    
-  ];
